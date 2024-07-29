20b501e87bef0e1424145437387e2e363df95ee3
gdeming <georgedeming@outlook.com>
2024-07-29 12:33:17 +0000

    debug

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     // 应用的命名空间
     'app_namespace'    => '',
     // 是否启用路由
-    'with_route'       => true,
+    'with_route'       => false,
     // 默认应用
     'default_app'      => 'index',
     // 默认时区
@@ -28,5 +28,5 @@ return [
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => false,
+    'show_error_msg'   => true,
 ];
