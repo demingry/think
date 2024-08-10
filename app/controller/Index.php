2fd51d79efb90be649a53c730bd3a86d0b33d277
gdeming <georgedeming@outlook.com>
2024-08-10 13:10:09 +0000

    0810

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -5,13 +5,19 @@ namespace app\controller;
 use app\BaseController;
 use think\facade\View;
 use think\facade\Request;
+use Util\data\Sysdb;
+use think\facade\Db;
 
 class Index extends BaseController
 {
-    public function index()
-    {
+    public function index($name=''){
         $name = Request::param('name');
         View::assign(['name'=>$name]);
         return View::fetch();
+    }
+
+    public function testdb(Sysdb $handler){
+        $name = $handler->table('test_table')->where(['id'=>1])->item();
+        dump($name);
     }
 }
